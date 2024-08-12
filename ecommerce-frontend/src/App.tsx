ae82b2cecc39623bf87c4eab46bd05203d90c553
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-12 21:17:49 +0300

    Added Admin panel dashboard page

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,17 +1,29 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Cart from './pages/cart';
-import Home from './pages/home';
-import Search from './pages/search';
+import { lazy, Suspense } from 'react';
+import Loader from './components/loader';
+
+const Cart = lazy(() => import('./pages/cart'));
+const Home = lazy(() => import('./pages/home'));
+const Search = lazy(() => import('./pages/search'));
+
+const Dashboard = lazy(() => import('./pages/admin/dashboard'));
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/search' element={<Search/>}/>
-                <Route path='/cart' element={<Cart/>}/>
-            </Routes>
-        </Router>
+        <Suspense fallback={<Loader />}>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/search' element={<Search />} />
+                    <Route path='/cart' element={<Cart />} />
+
+                    <Route>
+                        <Route path='/admin/dashboard' element={<Dashboard />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </Suspense >
+
     )
 }
 
