321e3dea20b2f236b1bdb1532d107b0d70706d95
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-16 22:02:04 +0300

    Added Cart item component

---
+++ ecommerce-frontend/src/components/cart-item.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { FaTrash } from 'react-icons/fa';
+
+type CartItemProps = {
+    cartItem: any;
+};
+
+const CartItem = ({ cartItem }: CartItemProps) => {
+    const { photo, name, productId, price, quantity } = cartItem;
+
+    return (
+        <div className="cart-item">
+            <img src={photo} alt={name} />
+            <article>
+                <Link to={`/product/${productId}`}>{name}</Link>
+                <span>{price}лв.</span>
+            </article>
+
+            <div>
+                <button>-</button>
+                <p>{quantity}</p>
+                <button>+</button>
+            </div>
+
+            <button>
+                <FaTrash />
+            </button>
+        </div>
+    );
+}
+
+export default CartItem;
