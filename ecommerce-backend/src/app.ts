38905d18076f312982ef369dcb49a9016e86c0db
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-19 08:33:46 +0300

    Added create new user controller

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { connectDB } from './utils/features.js';
 import { config } from 'dotenv';
 
+import userRoute from './routes/user.js';
+
 config({
     path: './.env',
 });
@@ -17,6 +19,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('API Working with /api/v1');
 })
+
+app.use('/api/v1/user', userRoute);
 
 app.listen(port, () => {
     console.log(`Server is working on http://localhost:${port}`);
