18539139e83a667c1836acd205d9f97e827fa473
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-20 16:38:33 +0300

    Added get all products controller

--- ecommerce-backend/src/controllers/product.ts
+++ ecommerce-backend/src/controllers/product.ts
@@ -2,7 +2,10 @@ import { Request } from 'express';
 import { TryCatch } from '../middlewares/error.js';
 import { redis, redisTTL } from '../app.js';
 import { Product } from '../models/product.js';
-import { NewProductRequestBody } from '../types/types.js';
+import {
+    BaseQuery,
+    NewProductRequestBody, SearchRequestQuery,
+} from '../types/types.js';
 import { uploadToCloudinary, } from '../utils/features.js';
 import ErrorHandler from '../utils/utility-class.js';
 
@@ -53,7 +56,7 @@ export const getlatestProducts = TryCatch(async (req, res, next) => {
 
     if (products) {
         products = JSON.parse(products);
-    } else {
+    }else {
         products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
         await redis.setex('latest-products', redisTTL, JSON.stringify(products));
     }
@@ -63,3 +66,64 @@ export const getlatestProducts = TryCatch(async (req, res, next) => {
         products,
     });
 });
+
+export const getAllProducts = TryCatch(
+    async (req: Request<{}, {}, {}, SearchRequestQuery>, res, next) => {
+        const { search, sort, category, price } = req.query;
+
+        const page = Number(req.query.page) || 1;
+
+        const key = `products-${search}-${sort}-${category}-${price}-${page}`;
+
+        let products;
+        let totalPage;
+
+        const cachedData = await redis.get(key);
+        if (cachedData) {
+            const data = JSON.parse(cachedData);
+            totalPage = data.totalPage;
+            products = data.products;
+        } else {
+            const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+            const skip = (page - 1) * limit;
+
+            const baseQuery: BaseQuery = {};
+
+            if (search)
+                baseQuery.name = {
+                    $regex: search,
+                    $options: "i",
+                };
+
+            if (price)
+                baseQuery.price = {
+                    $lte: Number(price),
+                };
+
+            if (category){
+                baseQuery.category = category;
+            }
+
+            const productsPromise = Product.find(baseQuery)
+                .sort(sort && { price: sort === "asc" ? 1 : -1 })
+                .limit(limit)
+                .skip(skip);
+
+            const [productsFetched, filteredOnlyProduct] = await Promise.all([
+                productsPromise,
+                Product.find(baseQuery),
+            ]);
+
+            products = productsFetched;
+            totalPage = Math.ceil(filteredOnlyProduct.length / limit);
+
+            await redis.setex(key, 30, JSON.stringify({ products, totalPage }));
+        }
+
+        return res.status(200).json({
+            success: true,
+            products,
+            totalPage,
+        });
+    }
+);
--- ecommerce-backend/src/routes/products.ts
+++ ecommerce-backend/src/routes/products.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getlatestProducts, newProduct } from '../controllers/product.js';
+import { getAllProducts, getlatestProducts, newProduct } from '../controllers/product.js';
 import { adminOnly } from '../middlewares/auth.js';
 import { mutliUpload } from '../middlewares/multer.js';
 
 const app = express.Router();
 
 app.post('/new', adminOnly, mutliUpload, newProduct);
+app.get('/all', getAllProducts);
 app.get("/latest", getlatestProducts);
 
 export default app;
