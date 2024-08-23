9045d8a6f8629c7f274be19ae51025c8f5a9da07
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-23 15:30:36 +0300

    Added change order status controller

--- ecommerce-backend/src/controllers/order.ts
+++ ecommerce-backend/src/controllers/order.ts
@@ -98,12 +98,12 @@ export const getSingleOrder = TryCatch(async (req, res, next) => {
     let order;
     order = await redis.get(key);
 
-    if (order){
+    if (order) {
         order = JSON.parse(order);
-    }else {
+    } else {
         order = await Order.findById(id).populate('user', 'name');
 
-        if (!order){
+        if (!order) {
             return next(new ErrorHandler('Order Not Found', 404));
         }
 
@@ -112,5 +112,42 @@ export const getSingleOrder = TryCatch(async (req, res, next) => {
     return res.status(200).json({
         success: true,
         order
+    });
+});
+
+export const processOrder = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+
+    if (!order){
+        return next(new ErrorHandler('Order Not Found', 404));
+    }
+
+    switch (order.status) {
+        case 'Processing':
+            order.status = 'Shipped';
+            break;
+        case 'Shipped':
+            order.status = 'Delivered';
+            break;
+        default:
+            order.status = 'Delivered';
+            break;
+    }
+
+    await order.save();
+
+    await invalidateCache({
+        product: false,
+        order: true,
+        admin: true,
+        userId: order.user,
+        orderId: String(order._id)
+    });
+
+    return res.status(200).json({
+        success: true,
+        message: 'Order Processed Successfully'
     });
 });
--- ecommerce-backend/src/routes/order.ts
+++ ecommerce-backend/src/routes/order.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { allOrders, getSingleOrder, myOrders, newOrder } from '../controllers/order.js';
+import { allOrders, getSingleOrder, myOrders, newOrder, processOrder } from '../controllers/order.js';
 import { adminOnly } from '../middlewares/auth.js';
 
 const app = express.Router();
@@ -7,6 +7,9 @@ const app = express.Router();
 app.post('/new', newOrder);
 app.get('/my', myOrders);
 app.get('/all', adminOnly, allOrders);
-app.route('/:id').get(getSingleOrder);
+app
+    .route('/:id')
+    .get(getSingleOrder)
+    .put(adminOnly, processOrder);
 
 export default app;
