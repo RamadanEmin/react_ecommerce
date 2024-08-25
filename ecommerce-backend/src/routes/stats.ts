e8cf93422c5ab3da305635d9af1e21b0ca3f7ccd
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-25 14:52:21 +0300

    Added get pie charts controller

--- ecommerce-backend/src/routes/stats.ts
+++ ecommerce-backend/src/routes/stats.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getDashboardStats } from '../controllers/stats.js';
+import { getDashboardStats, getPieCharts } from '../controllers/stats.js';
 import { adminOnly } from '../middlewares/auth.js';
 
 const app = express.Router();
 
 app.get('/stats', adminOnly, getDashboardStats);
+app.get('/pie', adminOnly, getPieCharts);
 
 export default app;
