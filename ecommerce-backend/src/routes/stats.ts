1a36a80c0dde7f8effec66b745d1b95e1fdd7dfa
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-26 09:47:52 +0300

    Added get line charts controller

--- ecommerce-backend/src/routes/stats.ts
+++ ecommerce-backend/src/routes/stats.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getBarCharts, getDashboardStats, getPieCharts } from '../controllers/stats.js';
+import { getBarCharts, getDashboardStats, getLineCharts, getPieCharts } from '../controllers/stats.js';
 import { adminOnly } from '../middlewares/auth.js';
 
 const app = express.Router();
@@ -7,5 +7,6 @@ const app = express.Router();
 app.get('/stats', adminOnly, getDashboardStats);
 app.get('/pie', adminOnly, getPieCharts);
 app.get('/bar', adminOnly, getBarCharts);
+app.get('/line', adminOnly, getLineCharts);
 
 export default app;
