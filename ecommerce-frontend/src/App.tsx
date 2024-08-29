1198ffbfc7247fcefa878081d07e0abf46bad5e6
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-29 16:00:12 +0300

    Added not found page

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -18,6 +18,7 @@ const Shipping = lazy(() => import('./pages/shipping'));
 const Login = lazy(() => import('./pages/login'));
 const Orders = lazy(() => import('./pages/orders'));
 const OrderDetails = lazy(() => import('./pages/order-details'));
+const NotFound = lazy(() => import('./pages/not-found'));
 
 const Dashboard = lazy(() => import('./pages/admin/dashboard'));
 const Products = lazy(() => import('./pages/admin/products'));
@@ -105,6 +106,7 @@ const App = () => {
 
                             <Route path='/admin/transaction/:id' element={<TransactionManagement />} />
                         </Route>
+                        <Route path='*' element={<NotFound />} />
                     </Routes>
                 </Suspense >
                 <Toaster position='bottom-center' />
