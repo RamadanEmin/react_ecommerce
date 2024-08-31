ca49b377739a287a51ac718590a47940839c0361
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-08-31 19:03:58 +0300

    Update cart item component

--- ecommerce-frontend/src/components/cart-item.tsx
+++ ecommerce-frontend/src/components/cart-item.tsx
@@ -1,32 +1,37 @@
 import { Link } from 'react-router-dom';
 import { FaTrash } from 'react-icons/fa';
+import { transformImage } from '../utils/features';
+import { CartItem } from '../types/types';
 
 type CartItemProps = {
-    cartItem: any;
+    cartItem: CartItem;
+    incrementHandler: (cartItem: CartItem) => void;
+    decrementHandler: (cartItem: CartItem) => void;
+    removeHandler: (id: string) => void;
 };
 
-const CartItem = ({ cartItem }: CartItemProps) => {
+const CartItemComponent = ({ cartItem, incrementHandler, decrementHandler, removeHandler }: CartItemProps) => {
     const { photo, name, productId, price, quantity } = cartItem;
 
     return (
         <div className="cart-item">
-            <img src={photo} alt={name} />
+            <img src={transformImage(photo)} alt={name} />
             <article>
                 <Link to={`/product/${productId}`}>{name}</Link>
-                <span>{price}лв.</span>
+                <span>₹{price}</span>
             </article>
 
             <div>
-                <button>-</button>
+                <button onClick={() => decrementHandler(cartItem)}>-</button>
                 <p>{quantity}</p>
-                <button>+</button>
+                <button onClick={() => incrementHandler(cartItem)}>+</button>
             </div>
 
-            <button>
+            <button onClick={() => removeHandler(productId)}>
                 <FaTrash />
             </button>
         </div>
     );
 }
 
-export default CartItem;
+export default CartItemComponent;
--- ecommerce-frontend/src/redux/store.ts
+++ ecommerce-frontend/src/redux/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { productAPI } from './api/productAPI';
 import { userAPI } from './api/userAPI';
+import { cartReducer } from './reducer/cartReducer';
 import { userReducer } from './reducer/userReducer';
 
 export const server = import.meta.env.VITE_SERVER;
@@ -9,7 +10,8 @@ export const store = configureStore({
     reducer: {
         [userAPI.reducerPath]: userAPI.reducer,
         [productAPI.reducerPath]: productAPI.reducer,
-        [userReducer.name]: userReducer.reducer
+        [userReducer.name]: userReducer.reducer,
+        [cartReducer.name]: cartReducer.reducer
     },
     middleware: (mid) => mid().concat(userAPI.middleware, productAPI.middleware)
 });
