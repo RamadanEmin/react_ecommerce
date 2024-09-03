62a99855f9f29027112c8e1d4bc281f36c899e88
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-09-03 21:59:04 +0300

    Added get stats api

--- ecommerce-frontend/src/redux/store.ts
+++ ecommerce-frontend/src/redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { dashboardApi } from './api/dashboardAPI';
 import { orderApi } from './api/orderAPI';
 import { productAPI } from './api/productAPI';
 import { userAPI } from './api/userAPI';
@@ -12,10 +13,11 @@ export const store = configureStore({
         [userAPI.reducerPath]: userAPI.reducer,
         [productAPI.reducerPath]: productAPI.reducer,
         [orderApi.reducerPath]: orderApi.reducer,
+        [dashboardApi.reducerPath]: dashboardApi.reducer,
         [userReducer.name]: userReducer.reducer,
         [cartReducer.name]: cartReducer.reducer
     },
-    middleware: (mid) => mid().concat(userAPI.middleware, productAPI.middleware, orderApi.middleware)
+    middleware: (mid) => mid().concat(userAPI.middleware, productAPI.middleware, orderApi.middleware, dashboardApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
