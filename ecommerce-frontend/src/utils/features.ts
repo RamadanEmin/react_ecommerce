e242f34e6a04ed430dd6dcb57c54d2f7985244d0
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-09-04 18:59:23 +0300

    Added get last months features

--- ecommerce-frontend/src/utils/features.ts
+++ ecommerce-frontend/src/utils/features.ts
@@ -3,6 +3,7 @@ import { MessageResponse } from '../types/api-types';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query/react';
 import { SerializedError } from '@reduxjs/toolkit';
 import { NavigateFunction } from 'react-router-dom';
+import moment from 'moment';
 
 export const transformImage = (url: string, width = 200) => {
     const newUrl = url.replace('upload/', `upload/dpr_auto/w_${width}/`);
@@ -32,4 +33,30 @@ export const responseToast = (
         const messageResponse = error.data as MessageResponse;
         toast.error(messageResponse.message);
     }
+};
+
+export const getLastMonths = () => {
+    const currentDate = moment();
+
+    currentDate.date(1);
+
+    const last6Months: string[] = [];
+    const last12Months: string[] = [];
+
+    for (let i = 0; i < 6; i++) {
+        const monthDate = currentDate.clone().subtract(i, 'months');
+        const monthName = monthDate.format('MMMM');
+        last6Months.unshift(monthName);
+    }
+
+    for (let i = 0; i < 12; i++) {
+        const monthDate = currentDate.clone().subtract(i, 'months');
+        const monthName = monthDate.format('MMMM');
+        last12Months.unshift(monthName);
+    }
+
+    return {
+        last12Months,
+        last6Months
+    };
 };
