dd6da631782d6cb86eb2638880fbe089c131a29d
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-09-08 12:22:29 +0300

    Import added pages

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -31,12 +31,11 @@ const Piecharts = lazy(() => import('./pages/admin/charts/piecharts'));
 const Linecharts = lazy(() => import('./pages/admin/charts/linecharts'));
 const Coupon = lazy(() => import('./pages/admin/apps/coupon'));
 const NewProduct = lazy(() => import('./pages/admin/management/newproduct'));
-const ProductManagement = lazy(
-    () => import('./pages/admin/management/productmanagement')
-);
-const TransactionManagement = lazy(
-    () => import('./pages/admin/management/transactionmanagement')
-);
+const ProductManagement = lazy(() => import('./pages/admin/management/productmanagement'));
+const TransactionManagement = lazy(() => import('./pages/admin/management/transactionmanagement'));
+const Discount = lazy(() => import('./pages/admin/discount'));
+const DiscountManagement = lazy(() => import('./pages/admin/management/discountmanagement'));
+const NewDiscount = lazy(() => import('./pages/admin/management/newdiscount'));
 
 const App = () => {
     const { user, loading } = useSelector((state: RootState) => state.userReducer);
@@ -97,18 +96,16 @@ const App = () => {
                             <Route path='/admin/product' element={<Products />} />
                             <Route path='/admin/customer' element={<Customers />} />
                             <Route path='/admin/transaction' element={<Transaction />} />
-
+                            <Route path='/admin/discount' element={<Discount />} />
                             <Route path='/admin/chart/bar' element={<Barcharts />} />
                             <Route path='/admin/chart/pie' element={<Piecharts />} />
                             <Route path='/admin/chart/line' element={<Linecharts />} />
-
                             <Route path='/admin/app/coupon' element={<Coupon />} />
-
                             <Route path='/admin/product/new' element={<NewProduct />} />
-
                             <Route path='/admin/product/:id' element={<ProductManagement />} />
-
                             <Route path='/admin/transaction/:id' element={<TransactionManagement />} />
+                            <Route path='/admin/discount/new' element={<NewDiscount />} />
+                            <Route path='/admin/discount/:id' element={<DiscountManagement />} />
                         </Route>
                         <Route path='*' element={<NotFound />} />
                     </Routes>
